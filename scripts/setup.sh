226fa7e0964893217841f475b3f6546d4e5b7648
Ivan Jacob Agaloos Pesigan <jeksterslab@users.noreply.github.com>
2024-09-01 15:39:44 -0400

    Automated build.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,8 @@
 set -e
 
 bash ubuntu_sys_pkg.sh
-# bash rstan.sh
-# bash rjags.sh
-# bash brms.sh
-# bash custom.sh
-# bash post.sh
+bash rstan.sh
+bash rjags.sh
+bash brms.sh
+bash custom.sh
+bash post.sh
